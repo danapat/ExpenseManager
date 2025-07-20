8a701a410d478224eb50b6fccd4cdd28966fea1c
danapat <bogdana.pat@gmail.com>
2025-07-20 21:05:26 +0200

    completed the save details API

---
+++ src/main/java/com/lushaj/ExpenseBackend/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package com.lushaj.ExpenseBackend.exceptions;
+
+import com.lushaj.ExpenseBackend.io.ErrorObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ErrorObject handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        return ErrorObject.builder()
+                .errorCode("DATA_NOT_FOUND")
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage())
+                .timestamp(new Date())
+                .build();
+
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(field -> field.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        errorResponse.put("statusCode", HttpStatus.BAD_REQUEST.value());
+        errorResponse.put("message", errors);
+        errorResponse.put("timestamp", new Date());
+        errorResponse.put("errorCode", "VALIDATION_FAILED");
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
