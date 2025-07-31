e65f962c333ee00424414673f96ea9aacb0c99dd
danapat <bogdana.pat@gmail.com>
2025-07-31 20:09:01 +0200

    completed the login API

---
+++ src/main/java/com/lushaj/ExpenseBackend/config/JwtRequestFilter.java
@@ -0,0 +1,62 @@
+package com.lushaj.ExpenseBackend.config;
+
+import com.lushaj.ExpenseBackend.service.impl.CustomUserDetailsService;
+import com.lushaj.ExpenseBackend.util.JwtTokenUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String requestTokenHeader = request.getHeader("Authorization");
+
+        String jwtToken = null;
+        String email = null;
+
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+            jwtToken = requestTokenHeader.substring(7);
+
+            try {
+                email = jwtTokenUtil.getUsernameFromToken(jwtToken);
+            }catch (IllegalArgumentException ex) {
+                throw new RuntimeException("Unable to get jwt token");
+            }catch (ExpiredJwtException ex) {
+                throw new RuntimeException("Jwt token is expired");
+            }
+
+        }
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+           UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+           if(jwtTokenUtil.validateToken(jwtToken, userDetails)) {
+               UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+               authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+               SecurityContextHolder.getContext().setAuthentication(authToken);
+           }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
